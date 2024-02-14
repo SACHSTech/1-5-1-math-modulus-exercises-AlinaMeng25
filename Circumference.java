d4914107fad8e1da4bd566fc4fefd3b61d892531
AlinaMeng25 <alina.meng25@ycdsbk12.ca>
2024-02-14 14:04:02 -0500

    finished circumference

--- Circumference.java
+++ Circumference.java
@@ -1,13 +1,24 @@
 class Circumference extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Curcumfrence from radius
+  * @author: Alina  :)
   */
   
   public void run() {
     
-    // start coding here
+    // variables
+    double dblRadius;
+    double dblCircumference;
+
+    // get radius
+    dblRadius = readDouble("What is the radius of thine circle?? ");
+
+    // calculation
+    dblCircumference = Math.round(2 * Math.PI * dblRadius);
+
+    // output results
+    System.out.println("The circumference of thine marvelous circle avec le radius " + dblRadius + " is about " + dblCircumference);
     
   }
 }

ece26bca3657280645b56c244d238a821203225f
AlinaMeng25 <alina.meng25@ycdsbk12.ca>
2024-02-16 13:50:54 -0500

    Finished program

--- Rads.java
+++ Rads.java
@@ -7,7 +7,18 @@ class Rads extends ConsoleProgram {
   
   public void run() {
     
-    // start coding here
+    // variables
+    double dblDegrees;
+    double dblRadians;
+
+    // data collection
+    dblDegrees = readDouble("How many degrees: ");
+
+    // calc
+    dblRadians = dblDegrees / 180;
+
+    // output
+    System.out.println(dblDegrees + " is " + dblRadians + "π in radians.");
     
   }
 }

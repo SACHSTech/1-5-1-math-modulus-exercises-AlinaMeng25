347ac724ff06a499292c02d2bf500eab02cd6085
AlinaMeng25 <alina.meng25@ycdsbk12.ca>
2024-02-20 16:45:56 -0500

    Finished Power

--- Power.java
+++ Power.java
@@ -1,13 +1,27 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Setting a number to a power
+  * @author: Alina Meng
   */
   
   public void run() {
     
-    // start coding here
+    // Variables
+    double dblBase;
+    double dblExp;
+
+    double dblPower;
+
+    // User Input
+    dblBase = readDouble("What is the base ");
+    dblExp = readDouble("What is the exponent ");
+
+    // Calc
+    dblPower = Math.pow(dblBase, dblExp);
+
+    // Output
+    System.out.println(dblBase + " to the exponent of " + dblExp + " is " + dblPower);
     
   }
 }

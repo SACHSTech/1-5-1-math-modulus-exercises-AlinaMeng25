d364ac782d02d3fdc0559fc8c2daa849836cf944
AlinaMeng25 <alina.meng25@ycdsbk12.ca>
2024-02-20 12:10:45 -0500

    Completed AreaSin

--- AreaSin.java
+++ AreaSin.java
@@ -1,13 +1,30 @@
 class AreaSin extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Finding the area of a triangle based on two sides and their contained angle
+  * @author: Alina Meng
   */
   
   public void run() {
     
-    // start coding here
+    // Variables
+    double dblSideA;
+    double dblSideB;
+    double dblAngleC;
+
+    double dblArea;
+
+    // user input
+    dblSideA = readDouble("What is the first side length in cm? ");
+    dblSideB = readDouble("What is the second side length in cm? ");
+    dblAngleC = readDouble("What is the contained angle in degrees? ");
+
+    // calcutations
+    dblAngleC = Math.toRadians(dblAngleC);
+    dblArea = (dblSideA*dblSideB*Math.sin(dblAngleC))/2;
+
+    // output
+    System.out.println("The area of this triangle is " + dblArea + "cm squared");
     
   }
 }

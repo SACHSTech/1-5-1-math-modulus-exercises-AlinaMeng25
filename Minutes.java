7db234accabd675ba19f0d76a123cc4662705889
AlinaMeng25 <alina.meng25@ycdsbk12.ca>
2024-02-20 17:20:21 -0500

    Finished Minutes

--- Minutes.java
+++ Minutes.java
@@ -1,13 +1,30 @@
 class Minutes extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Minutes to days and hours
+  * @author: Alina Meng
   */
   
   public void run() {
     
-    // start coding here
+    // Variables
+    int intMins;
+    int intHours;
+    int intDays;
+
+    int intRemainMins;
+
+    // User Input
+    intMins = readInt("How many minutes? ");
+
+    // Calculation
+    intDays = (int)intMins/1440;
+    intRemainMins = intMins%1440;
+    intHours = (int)((intMins - intDays*1440)/60);
+    intRemainMins = intRemainMins%60;
+
+    // Output
+    System.out.println(intMins + " minutes = " + intDays + " days, " + intHours + " hours, and " + intRemainMins + " minutes");
     
   }
 }

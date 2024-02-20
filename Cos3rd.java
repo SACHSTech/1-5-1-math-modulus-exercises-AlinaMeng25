0744420539e1fdf50a9376ae815a6abd73b301e3
AlinaMeng25 <alina.meng25@ycdsbk12.ca>
2024-02-20 11:13:05 -0500

    Completed Cos

--- Cos3rd.java
+++ Cos3rd.java
@@ -7,7 +7,23 @@ class Cos3rd extends ConsoleProgram {
   
   public void run() {
     
-    // start coding here
+    // Variables
+    double dblSideA;
+    double dblSideB;
+    double dblAngleC;
+
+    double dblSideC;
+
+    // user input
+    dblSideA = readDouble("What is the first side length in cm? ");
+    dblSideB = readDouble("What is the second side length in cm? ");
+    dblAngleC = readDouble("What is the contained angle in degrees? ");
+
+    // calcutations
+    dblSideC = Math.sqrt(Math.pow(dblSideA, 2) + Math.pow(dblSideB, 2) - 2*dblSideA*dblSideB * Math.cos(dblAngleC));
+
+    // output
+    System.out.println("The length of your third angle is around " + Math.round(dblSideC) + "cm.");
     
   }
 }
